d8589521b6cc80ec92a5d1a77061813e436e760b
clare-office <1062186165@qq.com>
2025-06-04 15:51:55 +0800

    fix bug

--- src/views/AiPPTX/Config.ts
+++ src/views/AiPPTX/Config.ts
@@ -1,1 +1,2 @@
-export const BackendApi = "http://localhost:8080/aipptx/"
+export const BackendApi = "https://fdzz.dandian.net:8443/aipptx/"
+//export const BackendApi = "http://localhost:8080/aipptx/"
